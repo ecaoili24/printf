cd90ab42516ea2680b63c38d1d7f4a77912f7044
kevapostol <apostolkevin@gmail.com>
2019-10-27 16:15:56 -0700

    Created printf function

---
+++ _printf.c
@@ -0,0 +1,36 @@
+int _printf(char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list arg;
+
+	va_start(arg, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+			case 'c':
+				count += c_print(va_arg(arg, int));
+				i++;
+				break;
+			case 's':
+				count += s_print(va_arg(arg, char *));
+				i++;
+				break;
+			case '%':
+				count += p_print();
+				i++;
+				break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(arg);
+	return (count);
+}
