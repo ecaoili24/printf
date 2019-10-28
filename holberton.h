af5100f9d59504585cc55e26c5d3a0f0c5d0dfb2
ecaoili24 <erika.caoili@gmail.com>
2019-10-28 23:52:59 +0000

    changed prototype

--- holberton.h
+++ holberton.h
@@ -9,6 +9,6 @@ int s_print(char *str);
 int p_print(void);
 int _printf(char *format, ...);
 int _printf_ext_1(va_list val, char ch, unsigned int *p_i);
-void id_print(int n);
+int id_print(int n, int count);
 
 #endif /* HOLBERTON_H */
